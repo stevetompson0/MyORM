57d891820054862457cd59a9ec0ee7799464508a
steve xu <stevexucityu@gmail.com>
2016-05-19 14:47:37 -0700

    use mysql Pool rather than connection; finish table function

--- MyORM.js
+++ MyORM.js
@@ -3,20 +3,53 @@
  */
 'use strict';
 
+
+const Promise = require('bluebird');
 const mysql = require('mysql');
+
+// Note that the library's classes are not properties of the main export
+// so we require and promisifyAll them manually
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+Promise.promisifyAll(require("mysql/lib/Pool").prototype);
 
 /**
  * MyORM -- a nodejs ORM for MySQL
- *
+ * TODO: dealing with errors
  */
 class MyORM {
 
   /**
    * constructor for the class
-   * @param connection -- object containing host, port, user, password, database
+   * @param data -- object containing a connection object with parameters: host, port, user, password, database
    */
-  constructor(connection) {
-    this.connection = mysql.createConnection({ connection});
+  constructor(data) {
+    if (typeof data == 'object' && typeof data.connection == 'object') {
+      this.pool = mysql.createPool(data.connection);
+    } else {
+      throw new Error('Invalid initial data');
+    }
+  }
+
+  /**
+   * table -- set the table to be queried
+   * @param tableName
+   */
+  table(tableName) {
+    this.table = tableName;
+    return this;
+  }
+
+  /**
+   * find -- a promise to make the query (table should be set before the query)
+   * @param query -- query object
+   */
+  find(query) {
+    if (typeof query == 'object') {
+
+      return this;
+    } else {
+      throw new Error('Invalid query format');
+    }
   }
 
 
--- tests/MyORMTests.js
+++ tests/MyORMTests.js
@@ -5,8 +5,21 @@
 
 const expect = require('chai').expect;
 
+const MyORM = require('../MyORM');
+const orm = new MyORM({
+  // mysql连接信息
+  connection: {host: '127.0.0.1', port: 3306, user: 'root', password: '', database: 'test'}
+});
+
 describe('MyORMTests', function() {
 
+  describe('find-query-tests', function() {
 
+    it('should return results', function() {
+
+    });
+
+
+  })
 
 });
