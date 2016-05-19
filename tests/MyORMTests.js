7e76a9e8c388824d8b93affd8ed4cc970b5d2acd
steve xu <stevexucityu@gmail.com>
2016-05-19 15:15:37 -0700

    fix test code based on airbnb lint rules

--- tests/MyORMTests.js
+++ tests/MyORMTests.js
@@ -9,18 +9,13 @@ const expect = require('chai').expect;
 const MyORM = require('../MyORM');
 const orm = new MyORM({
   // mysql连接信息
-  connection: {host: '127.0.0.1', port: 3306, user: 'root', password: '', database: 'test'}
+  connection: { host: '127.0.0.1', port: 3306, user: 'root', password: '', database: 'test' }
 });
 
-describe('MyORMTests', function() {
-
-  describe('find-query-tests', function() {
-
-    it('should return results', function() {
+describe('MyORMTests', () => {
+  describe('find-query-tests', () => {
+    it('should return results', () => {
 
     });
-
-
-  })
-
+  });
 });
